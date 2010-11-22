19194a2570dd99a0ea7e2f3f37867b80e837bd91
Zachariah Dumey <zachariahdumey@gmail.com>
2010-11-22 13:50:49 -0600

    added our names to the tsh.c file... you need to add your email, colin

--- tsh.c
+++ tsh.c
@@ -2,6 +2,8 @@
  * tsh - A tiny shell program with job control
  * 
  * <Put your name and login ID here>
+ * Colin McDonough <email here>
+ * Zachariah Dumey zwdumey@wustl.edu
  */
 #include <stdio.h>
 #include <stdlib.h>
